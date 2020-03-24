e69d8fbe972543f0d5e0be93d09b571d1ed67389
Viktor Kleen <viktor@kleen.org>
2020-03-24 17:39:26 +0000

    fix update-boot

--- chlorine/vkleen/scripts.nix
+++ chlorine/vkleen/scripts.nix
@@ -3,8 +3,8 @@ let update-boot = pkgs.writeScriptBin "update-boot" ''
       #!${pkgs.stdenv.shell}
       sudo cp $(realpath /nix/var/nix/profiles/system/kernel) /boot/
       sudo cp $(realpath /nix/var/nix/profiles/system/initrd) /boot/
-      sudo tee /boot/cmdline <<EOF
-        init=$(realpath /nix/var/nix/profiles/system/init) $(< /nix/var/nix/profiles/system/kernel-params
+      sudo tee /boot/cmdline >/dev/null <<EOF
+        init=$(realpath /nix/var/nix/profiles/system/init) $(< /nix/var/nix/profiles/system/kernel-params)
       EOF
     '';
 in {

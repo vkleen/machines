f739e3d3fdad799ef882f7ac0399b5596abddb01
Viktor Kleen <viktor@kleen.org>
2020-04-09 16:19:40 +0000

    seaborgium: Make freecad libspnav overlay more compatible

--- seaborgium/overlays/freecad.nix
+++ seaborgium/overlays/freecad.nix
@@ -1,13 +1,5 @@
 self: super: {
   freecad = super.freecad.overrideAttrs (o: {
-    buildInputs = with self; [
-      cmake coin3d xercesc ode eigen opencascade-occt gts
-      zlib swig gfortran soqt libf2c makeWrapper openmpi vtk hdf5 medfile
-      libGLU xorg.libXmu libspnav
-    ] ++ (with self.qt5; [
-      qtbase qttools qtwebkit qtx11extras
-    ]) ++ (with self.python3Packages; [
-      matplotlib pycollada shiboken2 pyside2 pyside2-tools pivy python boost
-    ]);
+    buildInputs = o.buildInputs ++ [ self.libspnav ];
   });
 }

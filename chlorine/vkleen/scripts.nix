603d33b0d1d5ec9ebd90388a934e7bf3a8de931a
Viktor Kleen <viktor@kleen.org>
2020-04-16 10:32:45 +0000

    chlorine: update-envrc script

--- chlorine/vkleen/scripts.nix
+++ chlorine/vkleen/scripts.nix
@@ -7,8 +7,19 @@ let update-boot = pkgs.writeScriptBin "update-boot" ''
         init=$(realpath /nix/var/nix/profiles/system/init) $(< /nix/var/nix/profiles/system/kernel-params)
       EOF
     '';
+
+    update-envrc = pkgs.writeScriptBin "update-envrc" ''
+      #!${pkgs.stdenv.shell}
+      SHELLNIX=''${1:-shell.nix}
+
+      mkdir -p .gcroots
+
+      ${pkgs.nix}/bin/nix-instantiate "''${SHELLNIX}" --indirect --add-root "$PWD"/.gcroots/shell.drv
+      ${pkgs.nix}/bin/nix-shell "$(readlink "$PWD"/.gcroots/shell.drv)" --run 'unset ''${!SSH_@} ''${!DIRENV_@} shellHook TEMP TEMPDIR TMP TMPDIR SSL_CERT_FILE NIX_SSL_CERT_FILE ''${!DBUS@} ''${!DESKTOP@} ''${!XDG@} ''${!TMUX@} I3SOCK TERM && ${pkgs.direnv}/bin/direnv dump bash > '"''${2:-.envrc.cache}"
+    '';
 in {
   home.packages = [
     update-boot
+    update-envrc
   ];
 }

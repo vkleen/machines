8a946dcdcf805df97e2410b88bbccc9fb2754b3e
Viktor Kleen <viktor@kleen.org>
2020-06-18 11:11:48 +0000

    remove firefox nightly

--- bohrium/vkleen/browsers.nix
+++ bohrium/vkleen/browsers.nix
@@ -2,9 +2,7 @@
 let
   cfg = config.browser;
 
-  firefox-pkg = (pkgs.latest.firefox-nightly-bin.override {
-    gdkWayland = true;
-  });
+  firefox-pkg = pkgs.firefox-bin;
 
   chromium-pkg = (pkgs.chromium.override {
     commandLineArgs = "--ssl-version-min=tls1 --cipher-suite-blacklist=0x009e,0x0033,0x0032,0x000a,0x0005,0x0004,0xc007,0xc011,0x009c,0x002f --disk-cache-dir=/tmp/cache --ozone-platform=wayland";

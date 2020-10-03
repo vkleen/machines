7cebf33fa5362ad5222a972d28f7d4f63ed4c7c3
Viktor Kleen <viktor@kleen.org>
2020-10-03 16:39:59 +0000

    add laptop udev-hacks

--- profiles/laptop/udev-hacks.nix
+++ profiles/laptop/udev-hacks.nix
@@ -5,6 +5,6 @@
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE:="0660", GROUP:="input", ATTR{power/control}="on"
 
     #DSLogic
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0be5|0bdd", MODE:="0660", GROUP:="kvm", ATTR{power/control}="on"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2a0e", ATTRS{idProduct}=="0003|0020", MODE:="0660", GROUP:="input"
   '';
 }

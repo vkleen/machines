eb144833ed12fa75f8beb5dd2767afe38606be5b
Viktor Kleen <viktor@kleen.org>
2020-10-11 14:47:09 +0000

    chlorine: reenable wireguard

--- hosts/chlorine/networking.nix
+++ hosts/chlorine/networking.nix
@@ -13,5 +13,20 @@
       "94.16.123.211"  = [ "samarium.kleen.org" ];
       "172.104.139.29" = [ "europium.kleen.org" ];
     };
+
+    wireguard.interfaces = {
+      wg2 = {
+        ips = [ "10.172.40.135/24" ];
+        privateKeyFile = "/run/keys/chlorine";
+        allowedIPsAsRoutes = false;
+        peers = [
+          { publicKey = builtins.readFile ../../secrets/wireguard/europium.pub;
+            allowedIPs = [ "0.0.0.0/0" "::/0" ];
+            endpoint = "europium.kleen.org:51820";
+            persistentKeepalive = 5;
+          }
+        ];
+      };
+    };
   };
 }

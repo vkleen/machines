0312656b14545afe18bd380f54d9afc678f54e54
Viktor Kleen <viktor@kleen.org>
2020-10-25 20:08:17 +0000

    weechat: update scripts

--- users/profiles/weechat/default.nix
+++ users/profiles/weechat/default.nix
@@ -3,46 +3,8 @@ let
   weechat-scripts = pkgs.fetchFromGitHub {
     owner = "weechat";
     repo = "scripts";
-    rev = "3e16849dd1a55902dbad9ac04176639190eedaf0";
-    hash = "sha256:0y57dbbalc31sj9z3dfk2a939ms19n4qfm5hqkrka395ij4s6frp";
-  };
-
-  weechat-discord = pkgs.rustPlatform.buildRustPackage {
-    pname = "weechat-discord";
-    version = "master";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "vkleen";
-      repo = "weechat-discord";
-      rev = "0d58c29774c3b806af4f5b3527cef8672cf47114";
-      hash = "sha256:0s8zjb3dqnsf6dhnax301ykhr8z8b65k0s1nlf11jk20n60ml5f9";
-    };
-
-    cargoSha256 = "1a08famirk4h66hr3xkcm9aidzw9w3c7ly12gdj488y81qlc690k";
-
-    buildInputs = [ pkgs.weechat-unwrapped pkgs.rust-bindgen ];
-
-    postConfigure = ''
-        export LIBCLANG_PATH="${pkgs.llvmPackages.clang-unwrapped.lib}/lib"
-        export BINDGEN_CFLAGS="$(< ${pkgs.stdenv.cc}/nix-support/libc-cflags) \
-          $(< ${pkgs.stdenv.cc}/nix-support/cc-cflags) \
-          ${pkgs.stdenv.cc.default_cxx_stdlib_compile} \
-          ${
-            lib.optionalString pkgs.stdenv.cc.isClang
-            "-idirafter ${pkgs.stdenv.cc.cc}/lib/clang/${
-              lib.getVersion pkgs.stdenv.cc.cc
-            }/include"
-          } \
-          ${
-            lib.optionalString pkgs.stdenv.cc.isGNU
-            "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${
-              lib.getVersion pkgs.stdenv.cc.cc
-            } -isystem ${pkgs.stdenv.cc.cc}/include/c++/${
-              lib.getVersion pkgs.stdenv.cc.cc
-            }/${pkgs.stdenv.hostPlatform.config}"
-          } \
-          $NIX_CFLAGS_COMPILE"
-    '';
+    rev = "25f7988014dedd508efa9a3df977c0da623693a9";
+    hash = "sha256-qo/+KZDg2A1zjbcFeiDSaRVTXSpKat0UdKmLMPXhMeQ=";
   };
 
   weechat-notify-send = pkgs.stdenv.mkDerivation {
@@ -91,10 +53,7 @@ let
   };
 in {
   options = {
-    weechat.weechat-discord = lib.mkOption {
-      default = weechat-discord;
-      type = lib.types.package;
-    };
+
   };
   config = {
     home.file.".weechat/python/autoload/weemustfeed.py".source =

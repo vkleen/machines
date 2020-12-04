a566dd341e8b79b473a50e0098d370f7dac9e77c
Viktor Kleen <viktor@kleen.org>
2020-12-04 00:22:38 +0000

    set the correct sandbox-paths for new nix

--- profiles/binfmt/default.nix
+++ profiles/binfmt/default.nix
@@ -8,16 +8,8 @@
       "riscv64-linux"
       "aarch64-linux"
     ];
-    registrations = {
-      aarch64-linux = {
-        fixBinary = true;
-      };
-      qemu-aarch64 = {
-        magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00'';
-        mask = ''\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\x00\xff\xfe\xff\xff\xff'';
-        fixBinary = true;
-        interpreter = (lib.systems.elaborate { system = "aarch64-linux"; }).emulator pkgs;
-      };
-    };
   };
+  nix.extraOptions = ''
+    sandbox-paths = ${toString config.nix.sandboxPaths}
+  '';
 }

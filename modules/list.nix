ca0b54202b438b6d334dcc587524205aed2d6db0
Viktor Kleen <viktor@kleen.org>
2020-12-06 23:22:00 +0000

    add mosquitto module

--- modules/list.nix
+++ modules/list.nix
@@ -1,6 +1,7 @@
 [
   ./boot/wipe-root.nix
   ./services/lock-on-suspend.nix
+  ./services/networking/mosquitto.nix
   ./services/uucp.nix
   ./tasks/netns.nix
   ./virtualisation/upstream-container.nix

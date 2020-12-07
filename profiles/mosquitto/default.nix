a695f2011cf1a2aaa8f89c7630ca34f63d3dcb8b
Viktor Kleen <viktor@kleen.org>
2020-12-07 23:47:56 +0000

    tweak mosquitto config

--- profiles/mosquitto/default.nix
+++ profiles/mosquitto/default.nix
@@ -4,43 +4,50 @@
     flake.nixosModules.mosquitto
     ../../secrets/mosquitto.nix
   ];
-  services.mosquitto = {
-    enable = true;
-    listeners = [
-      { host = "localhost";
-        port = 1883;
-      }
-      { host = "localhost";
-        port = 8883;
-        ssl = {
-          enable = true;
-          cafile = "/persist/mosquitto/ca.pem";
-          certfile = "/persist/mosquitto/mqtt.pem";
-          keyfile = "/persist/mosquitto/mqtt.key";
+  services.mosquitto = lib.mkMerge [
+    {
+      enable = true;
+      listeners = [
+        { host = "localhost";
+          port = 1883;
+        }
+        { host = "localhost";
+          port = 8883;
+          ssl = {
+            enable = true;
+            cafile = "/persist/mosquitto/ca.pem";
+            certfile = "/persist/mosquitto/mqtt.pem";
+            keyfile = "/persist/mosquitto/mqtt.key";
+          };
+        }
+      ];
+      users = {
+        "relayd" = {
+          acl = [
+            "topic readwrite relays/+/status"
+            "topic readwrite relays/+/+/diagnostic"
+            "topic read relays/+/+/state"
+          ];
         };
-      }
-    ];
-    users = {
-      "relayd" = {
-        acl = [
-          "topic readwrite relays/+/status"
-          "topic readwrite relays/+/+/diagnostic"
-          "topic read relays/+/+/state"
-        ];
+        "root" = {
+          acl = [
+            "topic readwrite #"
+            "topic readwrite $SYS/#"
+          ];
+        };
       };
-      "root" = {
-        acl = [
-          "topic readwrite #"
-        ];
-      };
-    };
-    allowAnonymous = false;
-    checkPasswords = true;
-  } // (if config.boot.wipeRoot then {
-    dataDir = "/persist/mosquitto";
-    extraConf = ''
-      persistence true
-      persistence_location /persist/mosquitto/data/
-    '';
-  } else {});
+      allowAnonymous = false;
+      checkPasswords = true;
+      extraConf = ''
+        sys_interval 1
+      '';
+    }
+    (lib.mkIf config.boot.wipeRoot {
+      dataDir = "/persist/mosquitto";
+      extraConf = ''
+        persistence true
+        persistence_location /persist/mosquitto/data/
+      '';
+    })
+  ];
 }

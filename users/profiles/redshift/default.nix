bcd58241027e80db6cdd61cd6c64cefbbc6519f3
Viktor Kleen <viktor@kleen.org>
2020-12-16 16:19:37 +0000

    switch redshift to gammmastep

--- users/profiles/redshift/default.nix
+++ users/profiles/redshift/default.nix
@@ -1,15 +1,22 @@
-{pkgs, ...}:
-{
+{pkgs, config, lib, ...}:
+let cfg = config.services.redshift;
+in {
   services.redshift = {
     enable = true;
     latitude = "51";
     longitude = "7";
-    tray = true;
+    tray = false;
     temperature = {
       day = 6500;
       night = 3500;
     };
-    package = pkgs.redshift-wlr;
-    extraOptions = [ "-m wayland" ];
+    package = pkgs.gammastep;
   };
+  systemd.user.services.redshift.Service.ExecStart = let
+    args = [
+      "-l ${cfg.latitude}:${cfg.longitude}"
+      "-t ${toString cfg.temperature.day}:${toString cfg.temperature.night}"
+      "-b ${toString cfg.brightness.day}:${toString cfg.brightness.night}"
+    ] ++ cfg.extraOptions;
+  in lib.mkForce "${cfg.package}/bin/gammastep ${lib.concatStringsSep " " args}";
 }

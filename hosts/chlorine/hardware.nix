71179fad53e3192615bae25a64373c47ca27795c
Viktor Kleen <viktor@kleen.org>
2021-02-02 19:20:29 +0000

    chlorine: add all crypto modules to initrd

--- hosts/chlorine/hardware.nix
+++ hosts/chlorine/hardware.nix
@@ -28,10 +28,23 @@
       };
     };
     cryptoModules = [
-      "dm_integrity" "aes" "aes_ti" "adiantum"
-      "aes_generic" "xts" "sha256" "sha512"
-      "dm_bufio" "algif_aead" "algif_skcipher" "md4"
-      "algif_hash" "arc4" "ctr" "cbc" "authenc" "cmac" "ccm"
+      "sm2_generic" "essiv" "ofb" "sha1_generic" "khazad" "hmac" "ansi_cprng"
+      "echainiv" "crypto_engine" "sm3_generic" "ghash-generic" "xxhash_generic"
+      "seed" "aegis128" "drbg" "michael_mic" "blowfish_generic" "blake2b_generic"
+      "salsa20_generic" "lrw" "algif_hash" "lz4hc" "blake2s_generic" "anubis"
+      "tgr192" "ecc" "blowfish_common" "rmd320" "crct10dif_common" "crypto_user"
+      "poly1305_generic" "rmd160" "adiantum" "crc32c_generic" "gf128mul" "md4"
+      "async_tx" "pcbc" "gcm" "twofish_generic" "arc4" "rsa_generic" "tea"
+      "twofish_common" "sha512_generic" "ecdh_generic" "crct10dif_generic"
+      "cast5_generic" "xts" "sha256_generic" "authenc" "chacha_generic"
+      "seqiv" "async_memcpy" "af_alg" "cast6_generic" "serpent_generic"
+      "algif_skcipher" "nhpoly1305" "xor" "ecb" "ecrdsa_generic" "rmd256"
+      "fcrypt" "wp512" "crc32_generic" "cts" "chacha20poly1305" "xcbc" "cmac"
+      "md5" "async_raid6_recov" "async_xor" "sha3_generic" "aes_generic" "keywrap"
+      "authencesn" "streebog_generic" "842" "cfb" "camellia_generic" "algif_aead"
+      "sm4_generic" "async_pq" "lz4" "cbc" "des_generic" "deflate" "rmd128"
+      "curve25519-generic" "aes_ti" "cast_common" "ccm" "jitterentropy_rng"
+      "algif_rng" "cryptd" "zstd" "vmac" "pcrypt" "ctr"
     ];
   };
 

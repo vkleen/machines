aa931aa18f3c22c276e7124853fd7d610f270fc2
Viktor Kleen <viktor@kleen.org>
2021-05-18 19:16:09 +0000

    enable xdg portal in sway profile

--- users/profiles/wayland/default.nix
+++ users/profiles/wayland/default.nix
@@ -193,7 +193,7 @@ in {
 
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
-    XDG_CURRENT_DESKTOP = "Unity";
+    XDG_CURRENT_DESKTOP = "sway";
     XDG_SESSION_TYPE = "wayland";
     WAYLAND_DISPLAY = "wayland-1";
   };

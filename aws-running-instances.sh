03d4cd8736b983caa54e8a617843e56c3324b8e5
Viktor Kleen <viktor@kleen.org>
2021-08-19 22:55:35 +0000

    aws-running-instances.sh: add dns name

--- aws-running-instances.sh
+++ aws-running-instances.sh
@@ -6,5 +6,5 @@ for r in $(aws ec2 describe-regions | \
             jq -r '.Regions[] | .RegionName' \
           ); do
   aws --region "$r" ec2 describe-instances --filters Name=instance-state-name,Values=running | \
-    jq -r '.Reservations[] | .Instances[] | "'"$r"': " + .InstanceId + " " + .InstanceType'
+    jq -r '.Reservations[] | .Instances[] | "'"$r"': " + .InstanceId + " " + .InstanceType + " " + .NetworkInterfaces[0].Association.PublicDnsName'
 done

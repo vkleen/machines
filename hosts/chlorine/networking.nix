d9d570c41eede809426091eedff892eeabb5a76f
Viktor Kleen <viktor@kleen.org>
2021-09-07 11:02:36 +0000

    update inputs

--- hosts/chlorine/networking.nix
+++ hosts/chlorine/networking.nix
@@ -14,19 +14,19 @@
       "172.104.139.29" = [ "europium.kleen.org" ];
     };
 
-    wireguard.interfaces = {
-      wg2 = {
-        ips = [ "10.172.40.135/24" ];
-        privateKeyFile = "/run/keys/chlorine";
-        allowedIPsAsRoutes = false;
-        peers = [
-          { publicKey = builtins.readFile ../../secrets/wireguard/europium.pub;
-            allowedIPs = [ "0.0.0.0/0" "::/0" ];
-            endpoint = "europium.kleen.org:51820";
-            persistentKeepalive = 5;
-          }
-        ];
-      };
-    };
+  #   wireguard.interfaces = {
+  #     wg2 = {
+  #       ips = [ "10.172.40.135/24" ];
+  #       privateKeyFile = "/run/keys/chlorine";
+  #       allowedIPsAsRoutes = false;
+  #       peers = [
+  #         { publicKey = builtins.readFile ../../secrets/wireguard/europium.pub;
+  #           allowedIPs = [ "0.0.0.0/0" "::/0" ];
+  #           endpoint = "europium.kleen.org:51820";
+  #           persistentKeepalive = 5;
+  #         }
+  #       ];
+  #     };
+  #   };
   };
 }

37e2aae90f366d88d72fc9ea5e8b51c3f40ec1f7
Viktor Kleen <viktor@kleen.org>
2021-09-07 23:29:10 +0000

    Use doas in chlorine extra-scripts

--- hosts/chlorine/extra-scripts.nix
+++ hosts/chlorine/extra-scripts.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 let
   update-boot = pkgs.writeShellScriptBin "update-boot" ''
-    sudo cp $(realpath /nix/var/nix/profiles/system/kernel) /boot/
-    sudo cp $(realpath /nix/var/nix/profiles/system/initrd) /boot/
-    sudo tee /boot/cmdline >/dev/null <<EOF
+    doas cp $(realpath /nix/var/nix/profiles/system/kernel) /boot/
+    doas cp $(realpath /nix/var/nix/profiles/system/initrd) /boot/
+    doas tee /boot/cmdline >/dev/null <<EOF
       init=$(realpath /nix/var/nix/profiles/system/init) $(< /nix/var/nix/profiles/system/kernel-params)
     EOF
   '';

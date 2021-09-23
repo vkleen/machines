829e15c98bbd10579bdd898242a7d70b2fcbaa6e
Viktor Kleen <viktor@kleen.org>
2021-09-23 20:16:31 +0000

    remove nushell

--- overlays/nushell.nix
+++
@@ -1,6 +0,0 @@
-final: prev: {
-  nushell = (prev.nushell.override { withStableFeatures = true; }).overrideAttrs (o: {
-    # buildInputs = o.buildInputs ++ [ final.xorg.libX11 final.xorg.libxcb final.python3 ];
-    cargoBuildFlags = "--features extra";
-  });
-}

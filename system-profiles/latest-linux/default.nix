124b7ee77b17620b8b2d163e58cafb45e5006b3d
Viktor Kleen <viktor@kleen.org>
2022-04-10 22:02:12 +0000

    Move to linux 5.17

--- system-profiles/latest-linux/default.nix
+++ system-profiles/latest-linux/default.nix
@@ -1,47 +1,4 @@
 { flake, flakeInputs, config, pkgs, lib, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_5_15.extend (self: super: {
-    kernel = with (import "${flakeInputs.nixpkgs}/lib/kernel.nix" { inherit lib; });
-      super.kernel.override {
-        structuredExtraConfig = {
-          PKCS8_PRIVATE_KEY_PARSER = yes;
-
-          BATMAN_ADV_NC = yes;
-
-          MAC80211_MESH = yes;
-
-          MOUSE_PS2_ELANTECH = yes;
-          MOUSE_PS2_ELANTECH_SMBUS = yes;
-          MOUSE_ELAN_I2C = module;
-          MOUSE_ELAN_I2C_I2C = no;
-          MOUSE_ELAN_I2C_SMBUS = yes;
-
-          POWERCAP = yes;
-          IDLE_INJECT = yes;
-          INTEL_RAPL = module;
-
-          USB_CONFIGFS_F_HID = yes;
-          USB_CONFIGFS_F_UVC = yes;
-          USB_CONFIGFS_MASS_STORAGE = yes;
-          USB_CONFIGFS_SERIAL = yes;
-
-          USB_F_HID = module;
-          USB_F_MASS_STORAGE = module;
-          USB_F_SERIAL = module;
-          USB_F_UVC = module;
-          USB_GADGETFS = module;
-          USB_GADGET_TARGET = module;
-
-          USB_G_HID = module;
-
-          USB_G_WEBCAM = module;
-          USB_MASS_STORAGE = module;
-
-          USB_U_SERIAL = module;
-
-          USBIP_CORE = module;
-          USBIP_VHCI_HCD = module;
-        };
-      };
-  });
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_5_17;
 }

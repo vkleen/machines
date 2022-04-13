cf92dc63d3162e8411aef27229fd95618b496a7d
Viktor Kleen <viktor@kleen.org>
2022-04-13 08:51:03 +0000

    direnv: use nix-direnv

--- home-modules/direnv/default.nix
+++ home-modules/direnv/default.nix
@@ -21,17 +21,8 @@ in
     enable = true;
     config = {
     };
-    stdlib = ''
-      : ''${XDG_CACHE_HOME:=$HOME/.cache}
-      envrc_dir=$(dirname $(find_up .envrc))
-      pwd_hash=$(basename $envrc_dir)-$(echo -n $envrc_dir | b2sum | cut -d ' ' -f 1)
-      direnv_layout_dir=$XDG_CACHE_HOME/direnv/layouts/$pwd_hash
-      mkdir -p $direnv_layout_dir
-
-      use_flake() {
-        watch_file "$(direnv_layout_dir)/flake-cache"
-        source "$(direnv_layout_dir)/flake-cache"
-      }
-    '';
+    nix-direnv = {
+      enable = true;
+    };
   };
 }

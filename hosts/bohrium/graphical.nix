ac58c591b10a3fadf9527a78787c9380ae73e186
Viktor Kleen <viktor@kleen.org>
2022-04-20 16:24:48 +0000

    bohrium/graphical: Use wlgreet

--- hosts/bohrium/graphical.nix
+++ hosts/bohrium/graphical.nix
@@ -1,7 +1,7 @@
 { config, flake, lib, pkgs, ... }:
 let
   greeter-sway-config = pkgs.writeText "sway-config" ''
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; ${pkgs.sway}/bin/swaymsg exit"
+    exec "${pkgs.greetd.wlgreet}/bin/wlgreet; ${pkgs.sway}/bin/swaymsg exit"
     bindsym Mod4+shift+e exec ${pkgs.sway}/bin/swaynag \
       -t warning \
       -m 'What do you want to do?' \
@@ -10,16 +10,16 @@ let
   '';
 in {
   environment.etc = {
-    "greetd/environments".text = ''
-      ${pkgs.sway}/bin/sway
-      ${pkgs.zsh}/bin/zsh
-    '';
+    "greetd/wlgreet.toml".source = (pkgs.formats.toml {}).generate "wlgreet.toml" {
+      command = "${pkgs.sway}/bin/sway";
+      outputMode = "all";
+      scale = 1;
+    };
   };
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        #command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.sway}/bin/sway";
         command = "${pkgs.sway}/bin/sway --config ${greeter-sway-config}";
       };
     };

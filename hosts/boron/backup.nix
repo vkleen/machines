abb25672f8b32aa06330bd974694d32eee9c21a2
Viktor Kleen <viktor@kleen.org>
2022-07-29 20:45:20 +0000

    boron: borg backups for paperless

--- hosts/boron/backup.nix
+++ hosts/boron/backup.nix
@@ -26,5 +26,19 @@ in {
       compression = "auto,lzma";
       startAt = "hourly";
     };
+
+    "paperless-rsync.net" = {
+      paths = [ "/persist/paperless" ];
+      doInit = true;
+      repo = "11414@ch-s011.rsync.net:paperless";
+      dateFormat = "-u +%s";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "${pkgs.coreutils}/bin/head -c-1 /run/agenix/boron-borg";
+      };
+      environment = { BORG_RSH = "ssh -oBatchMode=yes -oIdentitiesOnly=yes -oUserKnownHostsFile=${knownHostsFile} -oHostKeyAlias=rsync -i /run/agenix/rsync"; };
+      compression = "auto,lzma";
+      startAt = "hourly";
+    };
   };
 }

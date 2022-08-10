4d0785b659356382a70da9648161241c94b1fb41
Viktor Kleen <viktor@kleen.org>
2022-08-10 18:04:02 +0000

    wolkenheim/bgp-uplink: fix import policies

--- system-profiles/wolkenheim/bgp-uplink.nix
+++ system-profiles/wolkenheim/bgp-uplink.nix
@@ -75,9 +75,6 @@ in {
         { prefix-set-name = "default-ipv6";
           prefix-list = [ { ip-prefix = "::/0"; } ];
         }
-        { prefix-set-name = "freerange-endpoint";
-          prefix-list = [ { ip-prefix = "206.83.40.91/32"; } ];
-        }
       ];
       defined-sets.neighbor-sets = [
         { neighbor-set-name = "uplink";
@@ -107,33 +104,19 @@ in {
         { name = "routes-import";
           statements = [
             { conditions.match-prefix-set = {
-                prefix-set = "freerange-endpoint";
-                match-set-options = "any";
-              };
-              conditions.bgp-conditions.route-type = "local";
-              actions.route-disposition = "accept-route";
-            }
-            { conditions.match-prefix-set = {
                 prefix-set = "default-ipv6";
                 match-set-options = "any";
               };
-              conditions.bgp-conditions.route-type = "local";
+              conditions.match-neighbor-set = {
+                neighbor-set = "uplink";
+                match-set-options = "any";
+              };
               actions.route-disposition = "accept-route";
             }
           ];
         }
         { name = "routes-export";
           statements = [
-            { conditions.match-neighbor-set = {
-                neighbor-set = "uplink";
-                match-set-options = "invert";
-              };
-              conditions.match-prefix-set = {
-                prefix-set = "freerange-endpoint";
-                match-set-options = "any";
-              };
-              actions.route-disposition = "accept-route";
-            }
             { conditions.match-neighbor-set = {
                 neighbor-set = "uplink";
                 match-set-options = "invert";
@@ -158,21 +141,7 @@ in {
               };
               actions.bgp-actions.set-next-hop = "self";
             }
-          ] ++ (lib.lists.map
-            (l: {
-              conditions.match-neighbor-set = {
-                neighbor-set = name l;
-                match-set-options = "any";
-              };
-              conditions.match-prefix-set = {
-                prefix-set = "freerange-endpoint";
-                match-set-options = "any";
-              };
-              actions.bgp-actions.set-next-hop = linkLocal_address
-                (ip4Namespace fabric normalizedLinks l.linkId)
-                hostName;
-            })
-            (linksInvolving hostName normalizedLinks));
+          ];
         }
       ];
     };

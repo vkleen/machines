3cdef2811e1441c3246ada48e70d6015ed74f2c9
Viktor Kleen <viktor@kleen.org>
2022-09-20 19:24:27 +0000

    Add allowed_signers file for git

--- accounts/vkleen@bohrium/git.nix
+++ accounts/vkleen@bohrium/git.nix
@@ -1,6 +1,11 @@
 { pkgs, nixos, ... }:
-{
+let
+  allowed-signers = pkgs.writeText "allowed_signers" ''
+    viktor@kleen.org,viktor.kleen@tweag.io sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDf2T6h9DvuFhqLnTYhsdNqnuVWJCWWo8o95/5LHWchIAAAABHNzaDo=
+  '';
+in {
   programs.git = {
+    extraConfig.gpg.ssh.allowedSignersFile = builtins.toString allowed-signers;
     includes = [
       {
         condition = "gitdir:~/work/tweag/";

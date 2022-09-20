cad2e1b52171ed7b7bff73d6533ee299e3392139
Viktor Kleen <viktor@kleen.org>
2022-09-20 19:25:38 +0000

    Remove github workflows for now
    Building the machines takes too long for github. I need a better caching
    solution and custom CI builders.

--- .github/workflows/adjust-nix-daemon-tmp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/systemd/system/nix-daemon.service.d /nix/tmp || true
-cp .github/workflows/nix-daemon-env.conf /etc/systemd/system/nix-daemon.service.d/
-systemctl daemon-reload
-systemctl restart nix-daemon

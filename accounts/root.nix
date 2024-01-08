f257e7ae60413b6fba5e59b4a2ecaf742c3283d9
Viktor Kleen <viktor@kleen.org>
2024-01-08 00:02:35 +0000

    Rotate passwords

--- accounts/root.nix
+++ accounts/root.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   users.users.root = {
     shell = lib.getExe pkgs.bash;
@@ -6,6 +6,11 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP03cNnW4bB4rqxfp62V1SqskfI9Gja0+EApP9//tz+b vkleen@arbro"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID4bSfqKF8Hw7SUoA+MEogjSXoqPbmqdud8LfKYbVA6UAAAABHNzaDo= vkleen@bohrium"
     ];
-    hashedPassword = "$6$rounds=500000$LOTAq.HWQYxy$lKdVbv3O7kER44KRcmVL6q5Ahvwi78CfLNVElX/KwXuqXsAu6L9NQ98Y2BWbkI9fHyuqr8lBfD30BTgikLhB20";
+    hashedPasswordFile = config.age.secrets."root-pass".path;
+  };
+
+  age.secrets."root-pass" = {
+    rekeyFile = ../secrets/root-pass.age;
+    owner = "root";
   };
 }

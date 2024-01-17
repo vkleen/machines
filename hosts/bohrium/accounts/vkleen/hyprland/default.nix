68a508f387fbc2b1d5a236ce67337b0c3386b0e2
Viktor Kleen <viktor@kleen.org>
2024-01-17 17:45:03 +0000

    bohrium: adjust hyprland animations

--- hosts/bohrium/accounts/vkleen/hyprland/default.nix
+++ hosts/bohrium/accounts/vkleen/hyprland/default.nix
@@ -188,12 +188,12 @@ in
           enabled = true;
           bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
           animation = [
-            "windows, 1, 2, myBezier"
-            "windowsOut, 1, 2, default, popin 80%"
-            "border, 1, 10, default"
+            "windows, 1, 3, default"
+            "windowsOut, 1, 3, default, popin 80%"
+            "border, 1, 2, default"
             "borderangle, 1, 8, default"
-            "fade, 1, 2, default"
-            "workspaces, 1, 2, default"
+            "fade, 1, 4, default"
+            "workspaces, 1, 3, default"
           ];
         };
         master = {

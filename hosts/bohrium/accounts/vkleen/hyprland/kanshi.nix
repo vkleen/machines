955c17b7d130e5b41626e9eea8e76eeac1941a2e
Viktor Kleen <viktor@kleen.org>
2024-02-04 19:55:42 +0000

    bohrium: kanshi tweaks

--- hosts/bohrium/accounts/vkleen/hyprland/kanshi.nix
+++ hosts/bohrium/accounts/vkleen/hyprland/kanshi.nix
@@ -26,10 +26,17 @@
             scale = 1.0;
           }
           {
-            criteria = "Unknown ASUS PB27U 0x0000388B";
+            criteria = "Synaptics Inc Non-PnP 0x00BC614E";
+            status = "enable";
+            mode = "1920x1080@60Hz";
+            position = "2256,0";
+            scale = 1.0;
+          }
+          {
+            criteria = "ASUSTek COMPUTER INC ASUS PB27U 0x0000388B";
             status = "enable";
             mode = "3840x2160@60Hz";
-            position = "3000,0";
+            position = "4176,0";
             scale = 1.0;
           }
         ];

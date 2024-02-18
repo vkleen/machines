aebfa4692be747800c8d973fd2cfb07e8401aa65
Viktor Kleen <viktor@kleen.org>
2024-02-18 18:26:16 +0000

    chlorine: remove forceRekeyOnSystem for agenix-rekey

--- hosts/chlorine/age.nix
+++ hosts/chlorine/age.nix
@@ -2,7 +2,6 @@
 {
   age.rekey = {
     hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGmcRnGSfx23U8d/OGsBB9x0raOlRe9BhzKyfOWpIS9n";
-    forceRekeyOnSystem = "x86_64-linux";
     agePlugins = lib.mkForce [ ];
   };
 }

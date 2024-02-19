6a319c9a897f609a245ca7268c0ee48550e48826
Viktor Kleen <viktor@kleen.org>
2024-02-19 08:21:48 +0000

    permit ssh root login

--- modules/profiles/ssh.nix
+++ modules/profiles/ssh.nix
@@ -1,19 +1,24 @@
-{ lib, config, inputs, ... }:
+{ lib, config, ... }:
 {
-  imports = with inputs.self.nixosModules; [
-    trilby.profiles.ssh
+  config = lib.mkMerge [
+    {
+      services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = lib.mkDefault false;
+      };
+    }
+    (lib.mkIf config.boot.wipeRoot {
+      services.openssh.hostKeys = [
+        {
+          path = "/persist/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/persist/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+      ];
+    })
   ];
-  config = lib.mkIf config.boot.wipeRoot {
-    services.openssh.hostKeys = [
-      {
-        path = "/persist/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/persist/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
-    ];
-  };
 }

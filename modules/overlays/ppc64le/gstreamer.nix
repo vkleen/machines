0dca4d98187955478c9ce5790cb4754d7f6ca607
Viktor Kleen <viktor@kleen.org>
2024-03-04 14:23:51 +0000

    ppc64le: disable openh264 in gst-plugins-bad

--- modules/overlays/ppc64le/gstreamer.nix
+++ modules/overlays/ppc64le/gstreamer.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ lib, ... }:
 final: prev: {
   gst_all_1 =
     prev.gst_all_1 // {
       gst-plugins-good = prev.gst_all_1.gst-plugins-good.overrideAttrs (o: {
         # aalib fails with unknown reference to "pow"
         mesonFlags = o.mesonFlags ++ [ "-Daalib=disabled" ];
+      });
+      # openh264 isn't supported on ppc64le
+      gst-plugins-bad = (prev.gst_all_1.gst-plugins-bad.override { openh264 = null; }).overrideAttrs (o: {
+        mesonFlags = o.mesonFlags ++ [ "-Dopenh264=disabled" ];
       });
     };
 }

3cfad1e67b8384c098b37e0954f06562a0cc49de
Viktor Kleen <viktor@kleen.org>
2024-03-24 15:18:06 +0000

    uranium: enable IOMMU

--- hosts/uranium/boot.nix
+++ hosts/uranium/boot.nix
@@ -1,7 +1,7 @@
-{ config, ... }:
+{ ... }:
 {
   config = {
-    boot.kernelParams = [ "console=ttyS0" ];
+    boot.kernelParams = [ "console=ttyS0" "intel_iommu=on" ];
     boot.loader.grub.enable = false;
   };
 }

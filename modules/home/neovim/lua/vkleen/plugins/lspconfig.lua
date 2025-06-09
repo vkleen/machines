6893906819aa4d4c110cbdff8f332ce6a950ef8e
Viktor Kleen <viktor@kleen.org>
2025-06-09 07:55:46 +0000

    nvim: supports_method deprecation warning

--- modules/home/neovim/lua/vkleen/plugins/lspconfig.lua
+++ modules/home/neovim/lua/vkleen/plugins/lspconfig.lua
@@ -42,7 +42,7 @@ return {
         vim.diagnostic.config({ virtual_text = false })
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
           local highlight_augroup = vim.api.nvim_create_augroup("vkleen-lsp-highlight", { clear = false })
           vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
             buffer = event.buf,
@@ -65,7 +65,7 @@ return {
           })
         end
 
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
           map("<leader>th", function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled, { bufnr = event.buf })
           end, "[T]oggle Inlay [H]ints")
